28f1bb379f47f8c0474db89f6ceb278e93df90d1
KoTe <kt@ktkt.kt>
2025-03-07 16:34:14 +0400

    something

--- lesson_1_6_step10.py
+++ lesson_1_6_step10.py
@@ -1,38 +1,45 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import unittest
 
-try:
-    link = "http://suninjuly.github.io/registration2.html"
-    browser = webdriver.Chrome()
-    browser.get(link)
+# from test_auto_stepik.lesson_2_4_step7 import browser
 
-    # Ваш код, который заполняет обязательные поля
-    input1 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your first name']")
-    input1.send_keys("Ivan")
-    input2 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your last name']")
-    input2.send_keys("Petrov")
-    input3 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your email']")
-    input3.send_keys("test@testest.test")
+class TestRegistration(unittest.TestCase):
+    def test_1(self):
+        self.link = "https://suninjuly.github.io/registration1.html"
+        self.browser = webdriver.Chrome()
+        self.browser.get(self.link)
+        first_name = self.browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your first name']")
+        first_name.send_keys("Ivan")
+        last_name = self.browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your last name']")
+        last_name.send_keys("Petrov")
+        email = self.browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your email']")
+        email.send_keys("test@testest.test")
+        button = self.browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+        time.sleep(1)
+        final_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertEqual('Congratulations! You have successfully registered!', final_text, 'Registration failed')
 
-    # Отправляем заполненную форму
-    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-    button.click()
+    def test_2(self):
+        self.link = "http://suninjuly.github.io/registration2.html"
+        self.browser = webdriver.Chrome()
+        self.browser.get(self.link)
+        first_name = self.browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your name']")
+        first_name.send_keys("Ivan")
+        last_name = self.browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your last name']")
+        last_name.send_keys("Petrov")
+        email = self.browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your email']")
+        email.send_keys("test@testest.test")
+        button = self.browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+        time.sleep(1)
+        final_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertEqual('Congratulations! You have successfully registered!', final_text, 'Registration failed')
 
-    # Проверяем, что смогли зарегистрироваться
-    # ждем загрузки страницы
-    time.sleep(1)
+    def tearDown(self):
+        self.browser.quit()
 
-    # находим элемент, содержащий текст
-    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-    # записываем в переменную welcome_text текст из элемента welcome_text_elt
-    welcome_text = welcome_text_elt.text
-
-    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-    assert "Congratulations! You have successfully registered!" == welcome_text
-
-finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(10)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+if __name__ == '__main__':
+    unittest.main()
